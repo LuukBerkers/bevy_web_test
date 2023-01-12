4526e7615047435bdf1163fa3fc8567fb241d944
Luuk <luukberkers@protonmail.com>
2023-01-12 13:54:40 +0100

    Automatically resize in browser

--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,17 @@ use bevy::prelude::*;
 
 fn main() {
     App::new()
-        .add_plugins(DefaultPlugins.set(ImagePlugin::default_nearest()))
+        .add_plugins(
+            DefaultPlugins
+                .set(WindowPlugin {
+                    window: WindowDescriptor {
+                        fit_canvas_to_parent: true,
+                        ..default()
+                    },
+                    ..default()
+                })
+                .set(ImagePlugin::default_nearest()),
+        )
         .add_startup_system(bevy_web_test::setup)
         .add_system(bevy_web_test::animate)
         .run();
